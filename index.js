0ad79f392734a2b8f1267955420360daeada23cf
jose-ant-1 <jgonsol1510@g.educaand.es>
2026-05-26 20:51:57 +0200

    mitad hecha

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import  {ccaa} from "./data/ccaa.js";
-
+import {ccaa} from "./data/ccaa.js";
 
 function mostrarComunidades(listaATomar = ccaa, orden) {
     let listaOrden = [...listaATomar];
@@ -11,24 +10,56 @@ function mostrarComunidades(listaATomar = ccaa, orden) {
     }
 
     return listaOrden.map(comunidad => `
-        <tr>
-        <td>${comunidad.nombre}</td>
-        <td>${comunidad.poblacion_total.toLocaleString()}</td>
+        <tr data-nombre="${comunidad.nombre}">
+            <td>${comunidad.nombre}</td>
+            <td>${comunidad.poblacion_total.toLocaleString()}</td>
         </tr>
     `).join("");
-
-
 }
+
 
 document.addEventListener("DOMContentLoaded", event => {
     const tabla = document.querySelector("tbody");
-    const orden = document.querySelector("#orden").value;
-    console.log(orden)
-    tabla.innerHTML = mostrarComunidades(ccaa, orden);
+    const orden = document.querySelector("#orden");
 
-    document.addEventListener("change", tabla.innerHTML = mostrarComunidades(ccaa, orden))
+    const inputCcAa = document.querySelector("#ccaa");
+    const inputCapital = document.querySelector("#capital");
+    const inputPresidente = document.querySelector("#presidente");
+    const inputProvincias = document.querySelector("#provincias");
+    const btnGuardar = document.querySelector("#btn-guardar");
 
+    function actualizarTabla() {
+        let valor = orden.value;
+        tabla.innerHTML = mostrarComunidades(ccaa, valor);
+    }
 
+    actualizarTabla();
+
+    orden.addEventListener("change", actualizarTabla);
+
+    tabla.addEventListener("click", (e) => {
+      const fila = e.target.closest("tr");
+
+      if (!fila) return;
+
+      const ccaaSelect = fila.dataset.nombre;
+      const comunidad = ccaa.find(c => c.nombre === ccaaSelect);
+
+      if (comunidad) {
+          inputCcAa.value = comunidad.nombre;
+          inputCapital.value = comunidad.capital;
+          inputPresidente.value = comunidad.presidente;
+
+          inputProvincias.value = comunidad.provincias.map(p => p.nombre).join("\n");
+
+          btnGuardar.disabled = false;
+
+          document.querySelectorAll("tr").forEach(tr => tr.classList.remove("seleccionada"));
+          fila.classList.add("seleccionada");
+
+      }
+
+    });
 
 })
 
