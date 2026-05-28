a14d2bce1d9092e35815e01e35198d56c8334ce3
Jose-ant-1 <jgonsol1510@g.educaand.es>
2026-05-28 14:28:22 +0200

    ejercicio 2 a hacer

--- ejercicio-2/index.js
+++ ejercicio-2/index.js
@@ -1,11 +1,35 @@
 import {datosUE} from "./data/uecountries.js"
 
 
+function tomarIdiomas(listaPaises = datosUE) {
+    const idiomas = new Set();
+    listaPaises.forEach(pais => {
 
+        idiomas.add(pais.idiomas.oficial);
+        if (pais.idiomas.otros_idiomas) {
+            const otros = pais.idiomas.otros_idiomas.split(',').map(idioma => idioma.trim());
+            otros.forEach(otro => idiomas.add(otro));
+        }
 
+    })
+    return Array.from(idiomas).sort();
+
+}
+
+function mostrarIdiomas() {
+    let idiomas = tomarIdiomas(datosUE);
+    return idiomas.map(idioma =>
+        `
+        <input id="${idioma}" type="radio"></input>
+        <label for="${idioma}">${idioma}</label>
+    `).join('');
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+    const divIdiomas = document.querySelector("#idiomas");
+    const checOficial = document.querySelector("#oficiales");
 
+    divIdiomas.innerHTML += mostrarIdiomas();
 
 
 })
